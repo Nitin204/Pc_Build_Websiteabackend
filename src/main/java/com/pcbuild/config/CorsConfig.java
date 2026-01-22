49793a6e0127291d0abad8b6cde8f6a2d62c9386
Nitin204 <nitinkatore686@gmail.com>
2026-01-22 13:02:05 +0530

    login

--- src/main/java/com/pcbuild/config/CorsConfig.java
+++ src/main/java/com/pcbuild/config/CorsConfig.java
@@ -12,7 +12,10 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
         .allowedOrigins(
                 "http://localhost:5173",	
-                "http://localhost:5174")            
+                "http://localhost:5174",
+                "https://pc-build-website.vercel.app",
+                "https://pc-build-websiteadmin.vercel.app"
+            )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
